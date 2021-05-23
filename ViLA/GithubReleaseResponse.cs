0fb9f7496fc28749ff062c9ce6706cc6ee41e975
Tyler Reynolds <me@reynolds.tj>
2021-05-23 14:06:55 -0700

    Silence compiler warnings
    This class is used for json parsing and will never be initialized on construction and it will always seem like the setters aren't used.

--- ViLA/GithubReleaseResponse.cs
+++ ViLA/GithubReleaseResponse.cs
@@ -1,19 +1,22 @@
 ﻿using Newtonsoft.Json;
 
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+
 namespace ViLA
 {
     public class GithubReleaseResponse
     {
         [JsonProperty("tag_name")]
-        public string TagName { get; set; }
+        public string TagName { get; set; } = null!;
 
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
         public bool Draft { get; set; }
 
         public bool Prerelease { get; set; }
 
         [JsonProperty("html_url")]
-        public string HtmlUrl { get; set; }
+        public string HtmlUrl { get; set; } = null!;
     }
 }
